bb41a8040ce637272504ea0846550b66b88992a1
Black <53127242+Aubert-co@users.noreply.github.com>
2021-05-28 23:33:24 -0300

    Update index.js

--- Back/route/index.js
+++ Back/route/index.js
@@ -1,5 +1,8 @@
 const route = require('express').Router()
-
+const data = new Date()
+const year = data.getFullYear()
+const month = data.getMonth()
+const day = data.getDay()
 
 route.get('/index',(req,res)=>{
     
