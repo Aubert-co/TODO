3f35b86bb758a90019d2800ac1266d31e47234a6
Black <53127242+Aubert-co@users.noreply.github.com>
2021-06-01 23:18:35 -0300

    Update index.js

--- Back/route/index.js
+++ Back/route/index.js
@@ -2,7 +2,7 @@ const route = require('express').Router()
 const data = new Date()
 const year = data.getFullYear()
 const month = data.getMonth()
-const day = data.getDay()
+const day = data.getDay()+1
 
 const mongo = require('../model/db')
 
@@ -18,12 +18,13 @@ route.post('/newTask',(req,res)=>{
     try{
     const {task_name,study_time} = req.body
 
+    console.log(typeof task_name,typeof study_time,typeof year,typeof day,typeof month)
     mongo.insertMany({
         task_name,
         study_time,
         day,
-        year,
-        month
+        month,
+        year
     })
 
     res.send({msg:'sucess'})
