87900a5b91c188aaed1ab8aae6b4b82780886cee
Black <53127242+Aubert-co@users.noreply.github.com>
2021-06-01 23:03:07 -0300

    Update db.js

--- Back/model/db.js
+++ Back/model/db.js
@@ -1,13 +1,17 @@
 const mongo = require('mongoose')
 
+require('dotenv').config()
 
-mongo.connect()
-const schema = {
-    Task_Name:String,
+mongo.connect(`${process.env.HOST_MONGO}`,{useUnifiedTopology:true,useNewUrlParser:true})
+
+const schema =new Schema {
+    task_Name:String,
     study_time:Number,
     day:Number,
     Month:Number,
     Year:Number
 }
 
-//module.exports = schema
+const TODO = mongo.model('TODO',schema)
+
+module.exports = TODO
