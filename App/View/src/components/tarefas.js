21b7ed6e26db2e59482680e5f872ae079151c731
Black <53127242+Aubert-co@users.noreply.github.com>
2021-08-16 23:19:37 -0300

    Update tarefas.js

--- App/View/src/components/tarefas.js
+++ App/View/src/components/tarefas.js
@@ -1,43 +1,16 @@
 import React,{useState,useEffect} from "react"
 import styled from 'styled-components'
 import Api from '../service/index'
+import Item from '../styles/index'
 const {ApiInsert,ApiSelectItemUncomplete,ApiDelete}  = Api
+
 const TarefasDiv = styled.div`
 width: 33%;
 background-color: white;
 border: 5px solid rgb(34, 150, 185);
 margin-left: 10px;
 `
-const Item = styled.div`
-height: 100%;
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: space-evenly;
-color: rgb(34, 150, 185);
-`
-const Add = styled.div`
-display: flex;
-justify-content: center;
-input{
-    width: 100%;
-    padding: 5%;
-    outline: none;
-    border: 5px solid rgb(34, 150, 185);
-    color: rgb(34, 150, 185);
-};
-button{
-    background-color: rgb(34, 150, 185);
-    width: 30%;
-    text-align: center;
-    color: white;
-    cursor: pointer;
-    margin-left: 5px;
-    font-family: 'Courier New', Courier, monospace;
-    font-size: 50px;
-}
 
-`
 
 //const App =()=><Btn>BUTTON</Btn>
 const AddTask = ()=>{
