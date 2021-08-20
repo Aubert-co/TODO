a6e6670b4c4ab18330052450cf71fb049c963284
Black <53127242+Aubert-co@users.noreply.github.com>
2021-08-19 23:30:09 -0300

    Update route.js

--- App/route/route.js
+++ App/route/route.js
@@ -3,12 +3,12 @@ const cors = require('cors')
 const body_P = require('body-parser')
 
 const index = require('./index')
-
+const completeTask = require('./completeTasks')
 
 route.use(cors())
 route.use(body_P.json())
 
 route.use(index)
-
+route.use(completeTask)
 
 module.exports =  route
