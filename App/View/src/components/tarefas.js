1ce56ed0898abca1c2f0b30aff1107d231972887
Black <53127242+Aubert-co@users.noreply.github.com>
2021-08-19 23:31:44 -0300

    Update tarefas.js

--- App/View/src/components/tarefas.js
+++ App/View/src/components/tarefas.js
@@ -2,22 +2,33 @@ import React,{useState,useEffect} from "react"
 import styled from 'styled-components'
 
 import Api  from '../service/index'
-import Item from '../styles/index'
+import TarefasDiv from '../styles/index'
 
 const {ApiInsert,ApiDelete,ApiSelectItemUncomplete} = Api
-const TarefasDiv = styled.div`
-width: 33%;
-background-color: white;
-border: 5px solid rgb(34, 150, 185);
-margin-left: 10px;
-`
 
 
 //const App =()=><Btn>BUTTON</Btn>
 const AddTask = ()=>{
     
 }
+const Select = ()=>{
+    const [selectValues,setSelect] = useState({value:5})
 
+    const changeSelect = ({target})=>{
+        setSelect({value:target.value})
+        console.log('ola')
+    }
+   return( <select  value={selectValues.value} onChange={changeSelect}>
+                            <option value="5">5m</option>
+                            <option value="10">10m</option>
+                            <option value="15">15m</option>
+                            <option value="30">30m</option>
+                            <option value="60">1hr</option>
+                            <option value="120">2hr</option>
+                            <option value="180">3hr</option>
+                            <option value="240">4hr</option>
+        </select>)
+}
 const ListTaskUncompplete =({datas})=>{
    
  
@@ -25,17 +36,21 @@ const ListTaskUncompplete =({datas})=>{
 }
 const SETvalues = async (setDatas)=>{
     const resp = await  ApiSelectItemUncomplete()
-    console.log(resp)
     setDatas(resp)
 }
+
 function Tarefas(){
     const [datas,setDatas] = useState([])
     const [obj,setObj] = useState({value:false})
     const [values,setValues] = useState("")
+  
     const sendValues = ()=>{
         ApiInsert(values)
         setObj({value:true})
         
+      }
+      const CompleteTask = ()=>{
+
       }
     const changeValues =({target})=>setValues(target.value)
     
@@ -47,30 +62,34 @@ function Tarefas(){
     useEffect(()=>{
         SETvalues(setDatas)
         if(obj.value=== true)()=>SETvalues(setDatas)
-
-     
-        
      },[obj])
   
 
    
-      const map = ({task_name,id})=><div className="done" key={id}>
-        <i className="material-icons" onClick={()=>DeleteTask(id)}>delete</i>
-     {task_name}
+    const map = ({task_name,id})=>
+    <div className="done open" key={id}>
+        <div className="itens">
+            <i className="material-icons" onClick={()=>DeleteTask(id)}>delete</i>
+            <i className="material-icons">create</i>
+            <i className="material-icons" onClick={e=>CompleteTask(id)} >check_box_outline_blank</i>
+        </div>
+                 <h3> {task_name}</h3>
+                <Select></Select>
      </div>
     
         
     return (
         
-        <TarefasDiv>
-            <Item>
+        <TarefasDiv >
+            <div className="item">
                 <h1>TODO LIST</h1>
-                <div className="item">
-                    <input type="text" onChange={changeValues} />
-                    <button onClick={sendValues}>+</button>
-                </div>
+                    <div className="add">
+                    
+                        <input type="text" onChange={changeValues} />
+                        <h1 onClick={sendValues}>+</h1>
+                    </div>
                 {datas.map(map)} 
-            </Item> 
+            </div> 
         </TarefasDiv>
     
     )
