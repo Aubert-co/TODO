9df4f1edf947ec600e3c55546480e9bf84635ada
Black <53127242+Aubert-co@users.noreply.github.com>
2021-08-19 23:31:38 -0300

    Update app.js

--- App/View/src/components/app.js
+++ App/View/src/components/app.js
@@ -2,22 +2,18 @@ import Tarefas from './tarefas'
 import TarefasComplete from './tarefasCompletes'
 import styled from 'styled-components'
 import React,{useState} from "react"
-const Main = styled.main`
 
-`
 const Container = styled.div`
-display: flex;
-flex-direction: column;
-height: 100vh;
-background-color: rgb(19, 73, 134);
+    flex-direction: column;
+    height: 100vh;
+    background-color: rgb(19, 73, 134);
 
 header{height: 20%;}
+footer{height: 10%;}
 main{
     height: 70%;
     display: flex;
 }
-footer{height: 10%;}
-
 `
 function App(){
 
