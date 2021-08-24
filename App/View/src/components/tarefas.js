7c1ea0f4894699cca2204ca6cefe3d9d598833cd
Black <53127242+Aubert-co@users.noreply.github.com>
2021-08-23 22:02:38 -0300

    Update tarefas.js

--- App/View/src/components/tarefas.js
+++ App/View/src/components/tarefas.js
@@ -3,66 +3,47 @@ import styled from 'styled-components'
 
 import Api  from '../service/index'
 import TarefasDiv from '../styles/index'
-
+import Select from './select'
 const {ApiInsert,ApiDelete,ApiSelectItemUncomplete,ApiCompleteTask} = Api
 
 
-//const App =()=><Btn>BUTTON</Btn>
-const AddTask = ()=>{
-    
-}
-const Select = ()=>{
-    const [selectValues,setSelect] = useState({value:5})
 
-    const changeSelect = ({target})=>{
-        setSelect({value:target.value})
-        console.log('ola')
-    }
-   return( <select  value={selectValues.value} onChange={changeSelect}>
-                            <option value="5">5m</option>
-                            <option value="10">10m</option>
-                            <option value="15">15m</option>
-                            <option value="30">30m</option>
-                            <option value="60">1hr</option>
-                            <option value="120">2hr</option>
-                            <option value="180">3hr</option>
-                            <option value="240">4hr</option>
-        </select>)
-}
-const ListTaskUncompplete =({datas})=>{
-   
- 
-    return <>{datas.map(map)} </>
-}
-const SETvalues = async (setDatas)=>{
+const ReceiveDatas = async (setDatas)=>{
     const resp = await  ApiSelectItemUncomplete()
     setDatas(resp)
 }
+
+const SendDatas = (name,values,setObj)=>{
+    switch(name){
+        case 'delete': 
+            ApiDelete(values.id)
+            break
+        case 'insert':
+            ApiInsert(values.name)
+            break
+        case 'complete':
+            ApiCompleteTask(values.id,values.time);
+            break;
+        default :console.log('error')
+    }
+    setObj({value:true})
+}
+
 
 function Tarefas(){
     const [datas,setDatas] = useState([])
     const [obj,setObj] = useState({value:false})
     const [values,setValues] = useState("")
-  
-    const sendValues = ()=>{
-        ApiInsert(values)
-        setObj({value:true})
-        
-      }
-      const CompleteTask = (id)=>{
-        ApiCompleteTask(id,'10')
-        setObj({value:true})
-      }
-    const changeValues =({target})=>setValues(target.value)
+    const [selectValues,setSelect] = useState(5)
     
-      const DeleteTask = (id)=>{
-        ApiDelete(id)
-        setObj({value:true})
-    }
-  
+    const changeValues =({target})=>setValues(target.value)
+
     useEffect(()=>{
-        SETvalues(setDatas)
-        if(obj.value=== true)()=>SETvalues(setDatas)
+        ReceiveDatas(setDatas)
+        if(obj.value=== true){
+            ReceiveDatas(setDatas)
+            
+        }
      },[obj])
   
 
@@ -70,12 +51,12 @@ function Tarefas(){
     const map = ({task_name,id})=>
     <div className="done open" key={id}>
         <div className="itens">
-            <i className="material-icons" onClick={()=>DeleteTask(id)}>delete</i>
+            <i className="material-icons" onClick={()=>SendDatas('delete',{id:id},setObj)}>delete</i>
             <i className="material-icons">create</i>
-            <i className="material-icons" onClick={e=>CompleteTask(id)} >check_box_outline_blank</i>
+            <i className="material-icons" onClick={()=>SendDatas('complete',{id:id,time:selectValues},setObj)} >check_box_outline_blank</i>
         </div>
-                 <h3> {task_name}</h3>
-                <Select></Select>
+                <h3> {task_name}</h3>
+                <Select setSelect={setSelect} selectValues={selectValues}></Select>
      </div>
     
         
@@ -87,7 +68,7 @@ function Tarefas(){
                     <div className="add">
                     
                         <input type="text" onChange={changeValues} />
-                        <h1 onClick={sendValues}>+</h1>
+                        <h1 onClick={()=>SendDatas('insert',{name:values},setObj)}>+</h1>
                     </div>
                 {datas.map(map)} 
             </div> 
