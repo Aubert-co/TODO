6cbfc40d4057e3168ead7a928fde9ece8b032bae
Black <53127242+Aubert-co@users.noreply.github.com>
2021-08-24 22:46:51 -0300

    Create myContext.js

---
+++ App/View/src/contexts/myContext.js
@@ -0,0 +1,11 @@
+import React,{useState,useContext} from "react"
+
+
+export const UpdateContext = React.createContext([false],()=>{})
+
+export const Update = ()=>{
+    const [updateElement,setUpdate] = useState(false)
+    return (
+        <></>
+    )
+}
