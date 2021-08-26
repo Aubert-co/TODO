d6f7dfdceb8c8511b74b1510d2ebed968bc69fbe
Black <53127242+Aubert-co@users.noreply.github.com>
2021-08-25 22:50:07 -0300

    Update tarefas.js

--- App/View/src/components/tarefas.js
+++ App/View/src/components/tarefas.js
@@ -21,19 +21,21 @@ const SendDatas = (name,values,setObj)=>{
         case 'insert':
             ApiInsert(values.name)
             break
-        case 'complete':
-            ApiCompleteTask(values.id,values.time);
-            break;
         default :console.error('error')
     }
     setObj({value:true})
 }
 
-
-export default function Tarefas({setUpdate}){
+const completeTask= ({id,time},setObj)=>{
+    setObj({update:true})
+    ApiCompleteTask(id,time);
+  
    
+ }
+export default function Tarefas({setUpdate}){
+    console.log('here tarefas')
     const [datas,setDatas] = useState([])
-    const [obj,setObj] = useState({value:false})
+    const [obj,setObj] = useState({value:false,update:false})
     const [values,setValues] = useState("")
     const [selectValues,setSelect] = useState(5)
     
@@ -41,22 +43,21 @@ export default function Tarefas({setUpdate}){
 
     useEffect(()=>{
         ReceiveDatas(setDatas)
-        if(obj.value=== true){
-            ReceiveDatas(setDatas)
-            setUpdate(true)
 
-        }
+        if(obj.update=== true)setUpdate(true)
+        
+       
        
      },[obj])
   
-
+   
    
     const map = ({task_name,id})=>
     <div className="done open" key={id}>
         <div className="itens">
             <i className="material-icons" onClick={()=>SendDatas('delete',{id:id},setObj)}>delete</i>
             <i className="material-icons">create</i>
-            <i className="material-icons" onClick={()=>SendDatas('complete',{id:id,time:selectValues},setObj)} >check_box_outline_blank</i>
+            <i className="material-icons" onClick={()=>completeTask({id:id,time:selectValues},setObj)} >check_box_outline_blank</i>
         </div>
                 <h3> {task_name}</h3>
                 <Select setSelect={setSelect} selectValues={selectValues}></Select>
