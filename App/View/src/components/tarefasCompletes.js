2ebe9f2e3914bdbd1427cebd69ec48e511d2852f
Black <53127242+Aubert-co@users.noreply.github.com>
2021-08-29 22:11:33 -0300

    Update tarefasCompletes.js

--- App/View/src/components/tarefasCompletes.js
+++ App/View/src/components/tarefasCompletes.js
@@ -1,30 +1,11 @@
 import React,{useState,useEffect,useContext} from "react"
-import {ApiSelectItemComplete} from '../service/index'
-import TarefasFeitas from '../styles/tarefascompletas'
-import MyContext from "./actions"
+import MyContext from "../contexts/actions"
 import DeleteButton from './BtnDelete'
 import { NameItens } from "./DivName"
+import {ApiSelectItemComplete} from '../service/index'
+import {Item,Done,TarefasFeitas} from '../styles'
 
 
-
-
-/*const NameItens = ({name,task_time})=>{
-        const time = task_time % 60 === 0 ? task_time/60 : task_time
-        const MinOurHrs = task_time % 60 === 0 ? 'hrs' : 'min'
-    return (
-     <div className="name">
-         <p>{name}</p>
-         <p> Tempo Gasto  {time + MinOurHrs}</p>
-     </div> 
-)}*/
-/*const DivDone = ({id,name,time,setUpdate})=>{
-   // const [ShowDeleteBtn,SetDeleteBtn] = useState(false)
-  
-    return (
-    <div className="done"  key={id} onMouseLeave={()=>SetDeleteBtn(false)} onMouseEnter={()=>SetDeleteBtn(true)}>
-      {ShowDeleteBtn===true ? <DeleteButton id={id} setUpdate={setUpdate}/> : <NameItens key={id} name={name} task_time={time} />}
-    </div>
-)}*/
 
 const ReceiveDatas =async (setDatas)=>{
     const resp = await ApiSelectItemComplete()
@@ -33,20 +14,21 @@ const ReceiveDatas =async (setDatas)=>{
 
 const MapTasks = ({datas,setUpdate})=>{
     const lastThreeTask = (array,lenght=3)=>array.length<3 ? array : array.slice(array.length-lenght,array.lenght)
-    const [ShowDeleteBtn,SetDeleteBtn] = useState(false)
+    
     const map = ({task_name,task_time,id})=>{
     return(
     //<DivDone key={id} id={id} time={task_time} name={task_name} setUpdate={setUpdate}/>
-        <div className="done"  key={id} onMouseLeave={()=>SetDeleteBtn(false)} onMouseEnter={()=>SetDeleteBtn(true)}>
-          {ShowDeleteBtn===true ? <DeleteButton id={id} setUpdate={setUpdate}/> :
-           <NameItens key={id} name={task_name} task_time={task_time} />}
-        </div>
+        <Done className="done"  key={id} >
+            
+            <DeleteButton id={id} Event={setUpdate}/> 
+            <NameItens key={id} name={task_name} task_time={task_time} />
+        </Done>
     )    
     }
     return lastThreeTask(datas).map(map)
 
 }
-export default function Item(){
+export default function TarefasComplete(){
     const [datas,setDatas] = useState([])
     const {setUpdate,updateElement} = useContext(MyContext)
     useEffect(()=>{
@@ -56,10 +38,10 @@ export default function Item(){
     
         return (
             <TarefasFeitas >
-                <div className="item">
+                <Item className="item">
                     <h1>Últimas Tarefas</h1>
                        <MapTasks datas={datas} setUpdate={setUpdate}/>
-                </div>
+                </Item>
             </TarefasFeitas>
         )
 }
