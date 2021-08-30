974ae8a0ff591206e4c54214ddb1fba4c68b5dc1
Black <53127242+Aubert-co@users.noreply.github.com>
2021-08-29 22:11:20 -0300

    Create BtnInsert.js

---
+++ App/View/src/components/BtnInsert.js
@@ -0,0 +1,18 @@
+import React,{useState} from 'react'
+import { ApiInsert } from '../service'
+import { Add } from '../styles'
+export default function BtnInsert({Event}){
+    const [values,setValues] = useState("")
+    const changeValues =({target})=>setValues(target.value)
+    
+    const SendDatas = (values)=>{
+            ApiInsert(values)
+            Event({update:true})
+        }
+return (
+        <Add className="add">
+                <input type="text" onChange={changeValues} />
+                <h1 onClick={()=>SendDatas(values)}>+</h1>
+        </Add>
+        )
+}
