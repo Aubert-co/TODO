13c9e1b4c4217b5ac4b0a11aba9b12ad764e00ee
Black <53127242+Aubert-co@users.noreply.github.com>
2021-08-29 22:11:42 -0300

    Update index.js

--- App/View/src/service/index.js
+++ App/View/src/service/index.js
@@ -1,16 +1,16 @@
-export const obj =  {
-   ApiSelectItemUncomplete:async function(){
-  const data = await fetch('http://localhost:8080/tasks/uncomplete')
+const Url = "http://localhost:8080/"
+  async function ApiSelectItemUncomplete(){
+  const data = await fetch(`${Url}tasks/uncomplete`)
   const {results} =await  data.json()
  
   return results
-},
-ApiSelectItemComplete:async function(){
+}
+async function  ApiSelectItemComplete(){
   const data = await fetch('http://localhost:8080/tasks/complete')
   const {results} = await data.json()
   return results
-},
-ApiDelete:async function (id){
+}
+async function ApiDelete (id){
   return fetch('http://localhost:8080/tasks',{
     method:'DELETE',
     body:JSON.stringify({id}),
@@ -18,40 +18,40 @@ ApiDelete:async function (id){
     
   })
   .then((resp)=>resp.json())
-},
+}
 
-ApiInsert:async function (task_name){
+async function ApiInsert (task_name){
+ 
   return fetch('http://localhost:8080/tasksinsert',{
     method:'POST',
     body:JSON.stringify({task_name}),
     headers:{'Content-Type':'application/json'}
   }) 
   .then((resp)=>resp.json())
-}, 
-ApiComplete:async function(id){
+}
+async function ApiComplete(id){
   return fetch('http://localhost:8080/tasks',{
     method:'DELETE',
     body:JSON.stringify({id}),
     headers:{'Content-Type':'application/json'}
   })
   .then((resp)=>resp.json())
-},
-ApiUpdate:async function({task_name}){
+}
+async function ApiUpdate({task_name}){
 const response = await fetch('http://localhost:8080/tasks',{
   method:'PUT', 
   body:JSON.stringify({id}),
   headers:{'Content-Type':'application/json'}
 })
  return response
-},
-ApiCompleteTask:async function (id,task_time){
+}
+async function ApiCompleteTask (id,task_time){
   const response = await fetch('http://localhost:8080/completetask',{
   method:'PUT',
   body:JSON.stringify({id,task_time}),
   headers:{'Content-Type':'application/json'}
 })
 }
-}
 
-//export  const obj =  {ApiSelectItemUncomplete,ApiDelete,ApiInsert,ApiUpdate,ApiComplete,ApiSelectItemComplete}
 
+export  {ApiComplete,ApiCompleteTask,ApiDelete,ApiInsert,ApiUpdate,ApiSelectItemComplete,ApiSelectItemUncomplete}
