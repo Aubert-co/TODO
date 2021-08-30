522e90cdc8bb0c02c991c06f4a13f8c324bb6768
Black <53127242+Aubert-co@users.noreply.github.com>
2021-08-29 22:11:24 -0300

    Update DivName.js

--- App/View/src/components/DivName.js
+++ App/View/src/components/DivName.js
@@ -1,12 +1,12 @@
 import React from 'react'
-
+import { Name } from '../styles'
 
 export  const NameItens = ({name,task_time})=>{
     const time = task_time % 60 === 0 ? task_time/60 : task_time
     const MinOurHrs = task_time % 60 === 0 ? 'hrs' : 'min'
 return (
- <div className="name">
+ <Name className="name">
      <p>{name}</p>
      <p> Tempo Gasto  {time + MinOurHrs}</p>
- </div> 
+ </Name> 
 )}
