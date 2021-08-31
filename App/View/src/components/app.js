8337252c84a6c2339bb172e712f6c64083d097e4
Black <53127242+Aubert-co@users.noreply.github.com>
2021-08-30 21:42:24 -0300

    Update app.js

--- App/View/src/components/app.js
+++ App/View/src/components/app.js
@@ -1,19 +1,25 @@
 import Tarefas from './tarefas'
 import TarefasComplete from './tarefasCompletes'
+import Recomendation from './recomendation'
 import React,{useContext, useState} from "react"
 import MyContext from '../contexts/actions'
+import UpdateElements from '../contexts/UpdateElements'
 import { Container } from '../styles/index'
 
 function App(){
     const [updateElement,setUpdate] = useState({update:false})
+    const [obj,setObj] = useState({UpdateAll:false,update:false})
     return (
         <Container>
         <header></header>
         
         <main>
             <MyContext.Provider value={{updateElement,setUpdate}}>
-                <TarefasComplete />
-                <Tarefas />
+                    <TarefasComplete />
+                <UpdateElements.Provider value={{obj,setObj}}>
+                    <Tarefas />
+                    <Recomendation />
+                </UpdateElements.Provider>
             </MyContext.Provider>
         </main>
 
