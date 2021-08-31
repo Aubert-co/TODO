71a033cf14bb5ff3e3affbea6770d86fe7bd2492
Black <53127242+Aubert-co@users.noreply.github.com>
2021-08-30 21:38:02 -0300

    Update index.js

--- App/route/index.js
+++ App/route/index.js
@@ -2,7 +2,9 @@ const app = require('express')()
 const db = require('../model/db')
 
 app
-    .get('/tasks/uncomplete',(req,res)=>{
+    .get('/selectUncomplete',(req,res)=>{
+
+        
         const FALSE = 0
         const sql = `SELECT * FROM tasks WHERE task_complete='${FALSE}'`
 
@@ -13,8 +15,11 @@ app
         })
     })
 
-    .post('/tasksinsert',(req,res)=>{
+    .post('/insert',(req,res)=>{
         const {task_name} = req.body
+
+        if(task_name === '' || typeof task_name !== 'string' )return res.status(404)
+        
         const sql = `INSERT into tasks(task_name,task_complete) VALUES('${task_name}','${0}')`
 
         db.query(sql,(err)=>{
@@ -23,39 +28,8 @@ app
             res.status(200).send({msg:'sucess'})
         })
     })
-    
-    .post('/tasks',(req,res)=>{
-        const date = new Date(),
-         Day = date.getDate(),
-         Mont = date.getMonth()+1,
-         Year = date.getFullYear(),
-         DATE_TODAY =`${Year}-${Mont}-${Day}`
-
  
-        const {task_name,task_complete,task_time} = req.body
-        const sql = `INSERT INTO tasks(task_name,task_date,task_complete) VALUES('${task_name}','${DATE_TODAY}','${task_complete}')`
-
-        db.query(sql,(err)=>{
-            if(err)throw err
-
-            res.status(200).send({msg:'sucess'})
-        })
-    })
-
-    .put('/tasks',(req,res)=>{
-        const {id,task_name,task_complete,task_time} = req.body
-
-        const sql = `UPDATE tasks SET task_name = '${task_name}' 
-        , task_complete = '${task_complete}' , task_time ='${task_time}'   WHERE id=${id}`
-
-        db.query(sql,(err)=>{
-            if(err)throw err
-
-            res.status(200).send({msg:'sucess'})
-        })
-    })
-
-    .delete('/tasks',(req,res)=>{
+    .delete('/delete',(req,res)=>{
         const {id} =  req.body
        
         const sql = `DELETE FROM tasks WHERE id='${id}'`
@@ -67,7 +41,32 @@ app
         })
    
     })
-
+    .get('/selectComplete',(req,res)=>{
+        const TRUE = 1
+        const sql = `SELECT * FROM tasks WHERE task_complete='${TRUE}' ORDER BY id ASC `
+        //const sql = "SELECT * FROM taskS WHERE task_date='2021-07-13'"
+        db.query(sql,(err,results)=>{
+            if(err)throw err
+    
+            res.status(200).send({msg:'sucess',results})
+        })
+    })
+    .put('/update',(req,res)=>{
+        const date = new Date(),
+         Day = date.getDate(),
+         Mont = date.getMonth()+1,
+         Year = date.getFullYear(),
+         DATE_TODAY =`${Year}-${Mont}-${Day}`
+    
+        const {id,task_time} = req.body
+        ,sql = `UPDATE tasks  SET task_date='${DATE_TODAY}' , task_complete = '1' , task_time='${task_time}' WHERE id='${id}'`
+    
+        db.query(sql,(err)=>{
+            if(err)throw err
+    
+            res.status(200).send({msg:'sucess'})
+        })
+    })
 
 module.exports = app
 
