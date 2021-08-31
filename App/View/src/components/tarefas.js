fa638a16ce4318bf86d2c9439408d3d956c1c027
Black <53127242+Aubert-co@users.noreply.github.com>
2021-08-30 21:47:25 -0300

    Update tarefas.js

--- App/View/src/components/tarefas.js
+++ App/View/src/components/tarefas.js
@@ -2,11 +2,11 @@ import React,{useState,useEffect,useContext} from "react"
 import MyContext from "../contexts/actions"
 import Select from './select'
 import DeleteButton from './BtnDelete'
-import BtnInsert from './BtnInsert'
+import {BtnInsert} from './BtnInsert'
 import BtnCompleteTasks from "./BtnCompleteTask"
 import {ApiSelectItemUncomplete} from '../service/index'
 import {Itens,Item,Done,TarefasDiv} from "../styles"
-
+import UpdateElement from '../contexts/UpdateElements'
 
 
 const ReceiveDatas = async (setDatas)=>{
@@ -19,8 +19,7 @@ const ReceiveDatas = async (setDatas)=>{
 export default function Tarefas(){
     const {setUpdate} = useContext(MyContext)
     const [datas,setDatas] = useState([])
-    const [obj,setObj] = useState({UpdateAll:false,update:false})
-    
+    const {obj,setObj} = useContext(UpdateElement)
     const [selectValues,setSelect] = useState(5)
     
   
@@ -35,21 +34,20 @@ export default function Tarefas(){
     const map = ({task_name,id})=>{
        
     return (
-    <Done className="done open" key={id}>
+    <Done className="done" key={id}>
         <Itens className="itens">
             <DeleteButton id={id} Event={setObj}/>
             <BtnCompleteTasks id={id} time={selectValues} Event={setObj}/>
-           
+            <Select setSelect={setSelect} selectValues={selectValues}></Select>
         </Itens>
-                <h3> {task_name}</h3>
-                <Select setSelect={setSelect} selectValues={selectValues}></Select>
+            <h3> {task_name}</h3>
      </Done>
      )
     }
         
     return (
         
-        <TarefasDiv >
+        <TarefasDiv className="tarefas">
             <Item className="item">
                 <h1>TODO LIST</h1>
                 <BtnInsert Event={setObj}/>
